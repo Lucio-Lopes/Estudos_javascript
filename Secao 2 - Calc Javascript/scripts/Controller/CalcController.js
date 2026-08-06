8e50a900edbb42c54698f848c099cbef2daf2f7a
Lucio Lopes <luciolopespinheiro@gmail.com>
2026-08-06 14:20:00 -0300

    Alterações no display calc

--- Secao 2 - Calc Javascript/scripts/Controller/CalcController.js
+++ Secao 2 - Calc Javascript/scripts/Controller/CalcController.js
@@ -1,34 +1,56 @@
 class CalcController{
 
     constructor(){
-
-        this._displayCalc = "0";
+        this._locale = "pt-BR";
+        this._displayCalcEl = document.querySelector("#display");
+        this._dateEl = document.querySelector("#data");
+        this._timeEl = document.querySelector("#hora");
         this._currentDate;
         this.initialize();
+
     }
 
-    initialize(){
+    initialize(){   
+        this.setDisplayDateTime();
 
-        let displayCalcEl = document.querySelector("#display");
-        let dateEl = document.querySelector("#data");
-        let hourEl = document.querySelector("#hora");
+        setInterval(()=>{
 
-        displayCalcEl.innerHTML = "4657";
-        dateEl.innerHTML = "05/08/2026";
-        hourEl.innerHTML = "14:00";
+            this.setDisplayDateTime();
+
+        }, 1000)
+
+    }
+
+    setDisplayDateTime(){
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+    }
+
+    get displayTime(){
+        return this._timeEl.innerHTML;
+    }
+    set displayTime(value){
+        this._timeEl.innerHTML = value;
+    }
+
+    get displayDate(){
+        return this._dateEl.innerHTML;
+    }
+    set displayDate(value){
+        this._dateEl.innerHTML = value;
     }
 
     get displayCalc(){
-        return this._displayCalc;
+        return this._displayCalcEl.innerHTML;
     }
     set displayCalc(value){
-        this._displayCalc = value;
+        this._displayCalcEl.innerHTML = value;
     }
     
-    get dataAtual(){
-        return this._currentDate;
+    get currentDate(){
+        return new Date();
     }
-    set dataAtual(value){
+    set currentDate(value){
         this._currentDate = value;
     }
 
