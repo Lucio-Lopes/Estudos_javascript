c15adb1f72e543ff7379b9acde4c45c1f2f330c4
Lucio Lopes <luciolopespinheiro@gmail.com>
2026-08-06 12:59:02 -0300

    First commit

---
+++ Secao 2 - Calc Javascript/scripts/Controller/CalcController.js
@@ -0,0 +1,35 @@
+class CalcController{
+
+    constructor(){
+
+        this._displayCalc = "0";
+        this._currentDate;
+        this.initialize();
+    }
+
+    initialize(){
+
+        let displayCalcEl = document.querySelector("#display");
+        let dateEl = document.querySelector("#data");
+        let hourEl = document.querySelector("#hora");
+
+        displayCalcEl.innerHTML = "4657";
+        dateEl.innerHTML = "05/08/2026";
+        hourEl.innerHTML = "14:00";
+    }
+
+    get displayCalc(){
+        return this._displayCalc;
+    }
+    set displayCalc(value){
+        this._displayCalc = value;
+    }
+    
+    get dataAtual(){
+        return this._currentDate;
+    }
+    set dataAtual(value){
+        this._currentDate = value;
+    }
+
+}
